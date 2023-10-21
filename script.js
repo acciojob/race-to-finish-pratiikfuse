9ba576155daaba136d0544b8c282907f6af2d460
Pratik Fuse <pratikfuse20@gmail.com>
2023-10-21 21:27:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 window.promises = [];
 
+for(let i = 1;i<=5;i++){
+	new Promise((res,rej)=>{
+		setTimeout(()=>{
+			res(i);
+		},i*1000);
+	})
+}
+
+document.getElementById("output").innerText = Promise.any(promises);
+
 // Do not change the code above this
 // add your promises to the array `promises`
